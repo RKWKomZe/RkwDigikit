e380cf4a2eea32b32a16b84173594867a9eb9275
Bastian Behr <behr@bergisch-media.de>
2018-01-16 09:23:50 +0100

    [INIT]

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,6 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+$_EXTKEY = 'rkw_digi_kit';
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript','RKW - DigiKit');
