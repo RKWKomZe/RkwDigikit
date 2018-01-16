bd76c2a5d9d8e82ac76b1d8267ab213746432b46
Bastian Behr <behr@bergisch-media.de>
2018-01-16 10:49:50 +0100

    [INIT]

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,2 +1,12 @@
 <?php
 defined('TYPO3_MODE') or die('Access denied!');
+
+$extGlobalConfiguration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['rkw_digi_kit']);
+
+if ((bool) $extGlobalConfiguration['isDev']) {
+    $developTSconfig = \TYPO3\CMS\Core\Utility\GeneralUtility::getUrl(
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY)
+        . 'Configuration/TSconfig/developTSconfig.t3s'
+    );
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig($developTSconfig);
+}
